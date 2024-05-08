aaefd4d4e6832ffd6bc84004b580728954c64c23
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-08 20:25:00 -0300

    requisito 1 passou

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    print("🍌")
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    count = 0
+    for entry, exit in permanence_period:
+        if not isinstance(entry, int) or not isinstance(exit, int):
+            return None
+
+        if entry <= target_time <= exit:
+            count += 1
+
+    return count
