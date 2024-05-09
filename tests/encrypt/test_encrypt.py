79140452ab74a264e85a14c09b64933dabaf5675
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-08 21:26:20 -0300

    requisito 2 foi

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("banana", "7")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(4, 4)
+
+    assert encrypt_message("banana", 7) == "ananab"
+
+    assert encrypt_message("banana", 1) == "b_anana"
+
+    assert encrypt_message("banana", 4) == "an_anab"
