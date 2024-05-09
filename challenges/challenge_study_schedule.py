de39645d744f1987d1c1f45e6e0b7a151be46eb6
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-08 22:05:17 -0300

    refatoração do requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,11 +3,11 @@ def study_schedule(permanence_period, target_time):
         return None
 
     count = 0
-    for entry, exit in permanence_period:
-        if not isinstance(entry, int) or not isinstance(exit, int):
+    for student in permanence_period:
+        if not isinstance(student[0], int) or not isinstance(student[1], int):
             return None
 
-        if entry <= target_time <= exit:
+        if student[0] <= target_time <= student[1]:
             count += 1
 
     return count
